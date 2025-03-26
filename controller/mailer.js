f88ee75d4d05da0cef7819aa713291a86a79dca5
Grace-babafemi <gracebabafemi993@gmail.com>
2025-03-26 09:07:51 +0100

    nodemailer changes

--- controller/mailer.js
+++ controller/mailer.js
@@ -18,8 +18,15 @@ async function verify(email, token) {
         from: process.env.EMAIL_USER,
         to: email,
         subject: "Verify your email",
-        html: `<p>Click the link below to verify your email: grace email</p>
-        ${verificationLink}`
+        html: `<div>
+        <p>Click the button below to verify your email:</p>
+        <a href="${verificationLink}" style="text-decoration: none;">
+          <button style="background-color: #2563eb; color: white; padding: 0.5rem 1rem; border-radius: 0.375rem; border: none; cursor: pointer; font-size: 1rem;" onmouseover="this.style.backgroundColor='#1d4ed8'" onmouseout="this.style.backgroundColor='#2563eb'">
+            Verify Email
+          </button>
+        </a>
+      </div>
+        `
     });
     console.log("An email was sent to :", email)
 };
